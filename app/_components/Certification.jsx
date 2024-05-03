48626ed9afd8a72dce8f74ea6ba90103c0598cdf
nugrohoarr <mit.shuki23@gmail.com>
2024-05-04 03:28:20 +0700

    feat: add certificates on home page

--- app/_components/Certification.jsx
+++ app/_components/Certification.jsx
@@ -15,20 +15,20 @@ import {
 
 export function Certification() {
   return (
-    <div className="relative grid place-content-center py-12 px-4 sm:py-12 sm:px-6  ">
+    <div className="relative grid place-content-center py-12 px-4 sm:py-12 sm:px-6">
       <Carousel
         opts={{
           align: "start",
         }}
-        className="flex gap-2 sm:gap-3 flex-wrap items-center justify-center rounded-md max-w-3xl mx-auto"
+        className="flex gap-2 sm:gap-3 flex-wrap items-center justify-center rounded-md max-w-3xl mx-auto relative"
       >
         <CarouselContent>
           {certicate.map((item, index) => (
             <CarouselItem key={index} className="basis-full sm:basis-1/2 lg:basis-1/3 p-1">
-              <Card className="border bg-background dark:bg-secondary">
-                <CardContent className="flex flex-col items-center justify-center p-3 sm:p-5">
-                  <img src={item.image} alt={item.title} className="w-3/4 sm:w-full max-w-sm h-auto mb-2"/>
-                  <p className="text-xs sm:text-sm text-center">{item.description}</p>
+              <Card className="border bg-background dark:bg-secondary card-mobile-small">
+                <CardContent className="flex flex-col items-center justify-center p-2 sm:p-5">
+                  <img src={item.image} alt={item.title} className="w-3/4 sm:w-full max-w-xs h-auto mb-2 img-mobile-small"/>
+                  <p className="text-xs text-center">{item.description}</p>
                   <p className="text-xs sm:text-sm text-primary mt-5">
                     <a href={item.credential || "/"} target="_blank" rel="noopener noreferrer">Credential</a>
                   </p>
@@ -37,9 +37,12 @@ export function Certification() {
             </CarouselItem>
           ))}
         </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
+        <CarouselPrevious className="CarouselPrevious" />
+        <CarouselNext className="CarouselNext" />
       </Carousel>
     </div>
   );
 }
+
+
+
